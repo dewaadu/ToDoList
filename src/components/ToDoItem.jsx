761f65b32a465c46ade8292579b4c890a2daebcf
dewaadu <dawitadane7@gmail.com>
2020-10-06 14:37:22 +0300

    "edit and delete"

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -3,14 +3,17 @@ import React, { useState } from 'react';
 
 function ToDoItems(props){
     const [isDone,setIsDone]=useState(false);
+    
     function crossItem(){
         setIsDone(prevValue=>{
             return !prevValue;
         });
     }
-
+const contentEdit={
+    contentEditable:"true"
+}
     return <div> 
-        <input className="checkbox" onClick={crossItem} type="checkbox"/> <p contentEditable="true" style={{textDecoration:isDone?"line-through":"none",fontWeight:isDone?"100":"none"}}>{props.text}</p></div>
+        <input className="checkbox" onClick={crossItem} type="checkbox"/> <p onDoubleClick={contentEdit} style={{textDecoration:isDone?"line-through":"none",fontWeight:isDone?"100":"none"}}>{props.text}</p></div>
        
 }
 export default ToDoItems;
